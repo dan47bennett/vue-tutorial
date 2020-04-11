0d42b1d7193b0cd46a95109f4f3effef3f125f5c
Dan Bennett <dghbennett47@gmail.com>
2020-04-11 17:36:46 +0100

    episode: event handling

--- main.js
+++ main.js
@@ -3,20 +3,36 @@ var app = new Vue({
     data: {
         product: "Socks",
         description: "Socks to show your mastery of Vue",
-        image: './assets/vmSocks-green-onWhite.jpg',
         link: 'https://www.amazon.co.uk/s?k=socks&ref=nb_sb_noss',
+        image: './assets/vmSocks-green-onWhite.jpg',
         inventory: 0,
         details: ["80% cotton", "20% polyester", "Gender-neutral"],
         variants: [
             {
                 variantId: 2234,
-                variantColour: "green"
+                variantColour: "green",
+                variantImage: './assets/vmSocks-green-onWhite.jpg'
             },
             {
                 variantId: 2235,
-                variantColour: "blue"
+                variantColour: "blue",
+                variantImage: './assets/vmSocks-blue-onWhite.jpg'
             }
         ],
-        sizes: ['Small', 'Medium', 'Large']
+        sizes: ['Small', 'Medium', 'Large'],
+        cart: 0
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        removeFromCart(){
+            if(this.cart > 0) {
+                this.cart -= 1
+            }
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
+        }
     }
 })
