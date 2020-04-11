ccb3c2af8948969908c9da5cf047899a43682de2
Dan Bennett <dghbennett47@gmail.com>
2020-04-11 14:52:15 +0100

    episode: attribute binding

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ var app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        description: "go on your feets"
+        description: "Socks to show your mastery of Vue",
+        image: './assets/vmSocks-green-onWhite.jpg',
+        link: 'https://www.amazon.co.uk/s?k=socks&ref=nb_sb_noss',
     }
 })
