f113f4aad853167459055aedcdf58fa4823d1030
Dan Bennett <dghbennett47@gmail.com>
2020-04-11 17:04:27 +0100

    episode: list rendering

--- main.js
+++ main.js
@@ -5,6 +5,18 @@ var app = new Vue({
         description: "Socks to show your mastery of Vue",
         image: './assets/vmSocks-green-onWhite.jpg',
         link: 'https://www.amazon.co.uk/s?k=socks&ref=nb_sb_noss',
-        inventory: 0
+        inventory: 0,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColour: "green"
+            },
+            {
+                variantId: 2235,
+                variantColour: "blue"
+            }
+        ],
+        sizes: ['Small', 'Medium', 'Large']
     }
 })
